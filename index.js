375b63002111d252cf91a8d26c0812be3a5e64dd
Allen Cheslik <allencheslik0@gmail.com>
2021-10-09 10:36:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// ToDo: npm init -y 
+// This will create yoru package-lock and package .json files
+
+// TODO: NPM install inquirer
+// This will get your Node Modules
+
+// Start Coding
+// ToDO: import inquirer (require())
+// ToDo const inquirer - require
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([{
+
+    }])
